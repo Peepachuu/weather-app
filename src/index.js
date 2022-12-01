825393ef5c1361beba4380f8dd59eb4ba26ce6b5
Zafir <zafirhasan2006@gmail.com>
2022-12-01 17:04:21 +0400

    Add functions for creating the bare bones of the display

--- src/index.js
+++ src/index.js
@@ -7,3 +7,43 @@ getWeatherData("London").then(data => {
 getWeatherData("BrozerLAnd").then(data => {
     displayWeatherData(data);
 });
+
+function CreateDisplay() {
+    const header = createHeader();
+    const main = createMainContent();
+
+    document.body.append(header, main);
+}
+
+function createHeader() {
+    const header = document.createElement("header");
+    const title = document.createElement("h1");
+    title.textContent = "Weather App";
+
+    header.appendChild(title);
+    return header;
+}
+
+function createMainContent() {
+    const main = document.createElement("section");
+    main.classList.add("main");
+
+    const searchSection = createSearchSection();
+    main.appendChild(searchSection);
+
+    return main;
+}
+
+function createSearchSection() {
+    const searchSection = document.createElement("section");
+    searchSection.classList.add("search");
+
+    const searchbar = document.createElement("input");
+    const searchButton = document.createElement("button");
+
+    searchButton.textContent = "Search";
+    searchSection.append(searchbar, searchButton);
+    return searchSection;
+}
+
+CreateDisplay();
